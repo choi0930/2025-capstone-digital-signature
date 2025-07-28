b5fcedde2e205f86bb6100c737b91594f4407520
choi0930 <0930alan@gmail.com>
2025-07-28 21:33:08 +0900

    csr생성 부분에서 개인키 로딩 실패시 NULL리턴으로 변경

--- client/make_csr.c
+++ client/make_csr.c
@@ -4,12 +4,12 @@
 #include <openssl/pem.h>
 #include <openssl/evp.h>
 
-EVP_PKEY *get_key(){
+EVP_PKEY *get_key(){ //private key
     FILE *fp = fopen("./client_key/ec_priv_key.pem", "r");
     
     if(!fp){
         perror("비밀키 파일 열기 실패");
-        return 0;
+        return NULL;
     }
 
     EVP_PKEY *pkey = PEM_read_PrivateKey(fp, NULL, NULL, NULL);
@@ -17,14 +17,19 @@ EVP_PKEY *get_key(){
 
     if(!pkey){
         fprintf(stderr, "비밀키 로딩 실패");
-        return 0;
+        return NULL;
     }
 
     return pkey;
 }
 
-X509_REQ *generate_csr(){
+X509_REQ *generate_csr(){ //csr 생성
     EVP_PKEY *pkey = get_key();
+    
+    if(pkey == NULL){
+        fprintf("key -> NULL");
+        return NULL;
+    }
 
     X509_REQ *req = X509_REQ_new();
 
@@ -44,17 +49,26 @@ X509_REQ *generate_csr(){
 
 }
 
-int main(){
-    //test
+int send_csr(int sockfd){
+    //csr요청 생성
     X509_REQ *req = generate_csr();
 
+    //BIO를 이용해 PEM 형식으로 메모리에 쓰기
     BIO *bio = BIO_new(BIO_s_mem());
-    PEM_write_bio_X509_REQ(bio, req);
+    if(!PEM_write_bio_X509_REQ(bio, req)){
+        fprintf(stderr, "x509 REQ -> PEM 변환 실패");
+        X509_REQ_free(req);
+        BIO_free(bio);
+        return -13;    
+    }
 
+    //BIO 버퍼에서 데이터 추출
     char *csr_pem;
     long csr_len = BIO_get_mem_data(bio, &csr_pem);
 
-    fwrite(csr_pem, 1, csr_len, stdout);
+    //fwrite(csr_pem, 1, csr_len, stdout);
+
+    
 
     BIO_free(bio);
     X509_REQ_free(req);
