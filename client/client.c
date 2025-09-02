2ae9503878aa36bdfc439d5b0477d4e77ae8ce2b
choi0930 <0930alan@gmail.com>
2025-09-02 21:03:53 +0900

    client 실행 했을때 서버 ip와 포트번호를 키보드를 통해 받도록 변경

--- client/client.c
+++ client/client.c
@@ -11,17 +11,30 @@ int main(int argc, char *argv[]) {
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd == -1){perror("socket"); exit(1);}
 
+    //접속할 서버 주소: 
+    //포트번호 : 
+    char server_ip[BUFFER_SIZE]; 
+    char port_num[7];
+    printf("접속할 서버 주소: ");
+    fgets(server_ip, BUFFER_SIZE, stdin);
+    server_ip[strcspn(server_ip, "\n")] = '\0';
+    
+    printf("포트번호 :"); //12345 -> server 54321 -> root_ca
+    fgets(port_num, 7, stdin);
+    port_num[strcspn(port_num, "\n")] = '\0';
+    
     // 2. 서버 주소 설정
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(argv[2]));
-	inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
+    server_addr.sin_port = htons(atoi(port_num));
+	inet_pton(AF_INET, server_ip, &server_addr.sin_addr);
 
     // 3. 서버에 연결
     if(connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
         perror("connect"); close(sockfd); exit(1);}
-    printf("[%s:%s 서버에 연결됨]\n", argv[1], argv[2]);
-    
+    printf("[%s:%s 서버에 연결됨]\n", server_ip, port_num);
+
+    /*--------------------------------------------------------------------------*/
     //인증서 전송
     printf("----------------------------\n");
     send_cert(sockfd); 
